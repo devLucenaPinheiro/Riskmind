045eca13dc0cbed86ca4470ef99ff5845487c67e
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2025-04-15 18:36:09 -0300

    Mudança de animação

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     setInterval(nextSlide, intervalTime);
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const items = document.querySelectorAll('.item-3');
+  
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+          }
+        });
+      }, {
+        threshold: 0.4
+      });
+      
+      document.querySelectorAll('.item-3, .card').forEach(el => observer.observe(el));
+    }
+)
