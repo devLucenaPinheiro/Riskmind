7c17c73997377d55345180ff92a8c87c1071c23a
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2025-05-07 19:41:55 -0300

    Ajustes para o carrossel de texto

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const slider = document.querySelector(".slider")
-    const slides = document.querySelectorAll(".slider img")
-    let currentIndex = 0
-    const intervalTime = 5000
-
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % slides.length
-        const nextScrollPosition = slides[currentIndex].offsetLeft
-        slider.scrollTo({ left: nextScrollPosition, behavior: "smooth" })
-    }
-    setInterval(nextSlide, intervalTime)
-})
-
 document.addEventListener("DOMContentLoaded", function() {
   
     const observer = new IntersectionObserver((entries) => {
@@ -109,21 +95,6 @@ const navLinks = document.querySelectorAll('#nav-list a')
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
     closeMenu()
-  })
-})
-
-const slider = document.querySelector('.slider')
-const botoes = document.querySelectorAll('.slider-nav a')
-
-botoes.forEach((botao, index) => {
-  botao.addEventListener('click', (e) => {
-    e.preventDefault()
-
-    const larguraSlide = slider.offsetWidth
-    slider.scrollTo({
-      left: larguraSlide * index,
-      behavior: 'smooth'
-    })
   })
 })
 
